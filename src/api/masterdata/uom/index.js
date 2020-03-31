703d56ee2a53bbb307a109faee22470163c80090
larjia <yongzhen.jiang@outlook.com>
2020-03-31 11:01:37 +0800

    add unit of measure (UOM) api

---
+++ src/api/masterdata/uom/index.js
@@ -0,0 +1,46 @@
+import request from '@/utils/request'
+
+export function listUOM (query) {
+  return request({
+    url: '/masterdata/uom/list',
+    method: 'get',
+    params: query
+  })
+}
+
+export function getUOMById (id) {
+  return request({
+    url: '/masterdata/uom/id/' + id,
+    method: 'get'
+  })
+}
+
+export function getUOMByNumber (number) {
+  return request({
+    url: '/masterdata/uom/number/' + number,
+    method: 'get'
+  })
+}
+
+export function addUOM (data) {
+  return request({
+    url: '/masterdata/uom',
+    method: 'post',
+    data: data
+  })
+}
+
+export function updateUOM (data) {
+  return request({
+    url: '/masterdata/uom',
+    method: 'put',
+    data: data
+  })
+}
+
+export function deleteUOMById (id) {
+  return request({
+    url: '/masterdata/uom/' + id,
+    method: 'delete'
+  })
+}
--- src/views/basedata/uom/index.vue
+++ src/views/basedata/uom/index.vue
@@ -1,10 +1,156 @@
 <template>
-    <h1>Unit of measure index view</h1>
+    <div class='app-container'>
+      <el-form :model='queryParams' ref='queryForm' :inline='true' label-width='68px'>
+        <el-form-item label='编码' prop='number'>
+          <el-input
+            v-model='queryParams.number'
+            clearable
+            size='small'
+            @keyup.enter.native='handelQuery'
+          />
+        </el-form-item>
+        <el-form-item label='名称' prop='name'>
+          <el-input
+            v-model='queryParams.name'
+            clearable
+            size='small'
+            @keyup.enter.native='handelQuery'
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type='primary' icon='el-icon-search' size='mini' @click='handleQuery'>搜索</el-button>
+          <el-button icon='el-icon-refresh' size='mini' @click="resetQuery">重置</el-button>
+        </el-form-item>
+      </el-form>
+
+      <el-row :gutter="10" class='mb8'>
+        <el-col :span="1.5">
+          <el-button
+            type='primary'
+            icon='el-icon-plus'
+            size='mini'
+            @click='handleAdd'
+          >新增</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type='success'
+            icon='el-icon-edit'
+            size='mini'
+            :disabled="single"
+            @click='handleUpdate'
+          >修改</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type='danger'
+            icon='el-icon-delete'
+            size='mini'
+            :disabled="multiple"
+            @click='handleDelete'
+          >修改</el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type='warning'
+            icon='el-icon-download'
+            size='mini'
+            @click='handleExport'
+          >修改</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table v-loading='loading' :data='uomList' @selection-change='handleSelectionChange' border size='small'>
+        <el-table-column type='selection' width='55' align='center' />
+        <el-table-column label='单位编码' align='center' prop='number' />
+        <el-table-column label='单位名称' align='center' prop='name' />
+        <el-table-column label='操作' align='center' class-name='small-padding fixed-width'>
+          <template slot-scope='scope'>
+            <el-button
+              size='mini'
+              type='text'
+              icon='el-icon-edit'
+              @click='handleUpdate(scope.row)'
+            >修改</el-button>
+            <el-button
+              size='mini'
+              type='text'
+              icon='el-icon-delete'
+              @click='handleDelete(scope.row)'
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <pagination
+        v-show='total > 0'
+        :total='total'
+        :page.sync='queryParams.pageNum'
+        :limit.sync='queryParams.pageSize'
+        @pagination='getList'
+      />
+    </div>
 </template>
 
 <script>
+import {
+  listUOM,
+  getUOMById,
+  getUOMByNumber,
+  addUOM,
+  updateUOM,
+  deleteUOM
+} from '@/api/masterdata/uom'
+
 export default {
-  name: 'UOM'
+  name: 'UOM',
+  data () {
+    return {
+       // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 总条数
+      total: 0,
+      // 计量单位表格数据
+      uomList: [],
+      // 弹出层标题
+      title: '',
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        number: undefined,
+        name: undefined
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+
+      }
+    }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    /** 查询列表 */
+    getList () {
+      this.loading = true
+      listUOM(this.queryParams).then(response => {
+        this.uomList = response.rows
+        this.total = response.total
+        this.loading = false
+      })
+    }
+  }
 }
 </script>
 
