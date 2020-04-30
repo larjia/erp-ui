5398f01a16ef7d3c06c1689b31a98ee2fb0757ab
Jiang <JiangL@winkelmann-group.com>
2020-04-30 09:40:43 +0800

    add role view

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -68,6 +68,7 @@
           size="mini"
           @click="handleUpdate"
           v-hasPermi="['system:role:edit']"
+          :disabled="!isSelected"
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -77,6 +78,7 @@
           size="mini"
           @click="handleDataScope"
           v-hasPermi="['system:role:edit']"
+          :disabled="!isSelected"
         >数据权限</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -85,6 +87,7 @@
           icon="el-icon-delete"
           size="mini"
           @click="handleDelete"
+          :disabled="!isSelected"
           v-hasPermi="['system:role:remove']"
         >删除</el-button>
       </el-col>
@@ -96,6 +99,13 @@
           @click="handleExport"
           v-hasPermi="['system:role:export']"
         >导出</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          size="mini"
+          @click="handleClear"
+          :disabled="!isSelected"
+        >取消选择</el-button>
       </el-col>
     </el-row>
 
@@ -120,6 +130,92 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改角色配置对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="form.roleName" placeholder="请输入角色名称" />
+        </el-form-item>
+        <el-form-item label="角色标识" prop="roleKey">
+          <el-input v-model="form.roleKey" placeholder="请输入角色标识" />
+        </el-form-item>
+        <el-form-item label="角色顺序" prop="roleSort">
+          <el-input-number v-model="form.roleSort" controls-position="right" :min="0" />
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.status">
+            <el-radio
+              v-for="dict in statusOptions"
+              :key="dict.dictValue"
+              :label="dict.dictValue"
+            >{{ dict.dictLabel }}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="菜单权限">
+          <el-tree
+            :data="menuOptions"
+            show-checkbox
+            ref="menu"
+            mode-key="id"
+            empty-text="加载中,请稍后"
+            :props="defaultProps"
+          ></el-tree>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确定</el-button>
+        <el-button @click="cancel">取消</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 分配角色数据权限对话框 -->
+    <el-dialog :title="title" :visible.sync="openDataScope" width="500px" append-to-body>
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="角色名称">
+          <el-input v-model="form.roleName" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="角色标识">
+          <el-input v-model="form.roleKey" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="权限范围">
+          <el-select v-model="form.dataScope">
+            <el-option
+              v-for="item in dataScopeOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="数据权限" v-show="form.dataScope == 2">
+          <el-tree
+            :data="deptOptions"
+            show-checkbox
+            default-expand-all
+            ref="dept"
+            node-key="id"
+            empty-text="加载中,请稍后"
+            :props="defaultProps"
+          ></el-tree>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitDataScope">确定</el-button>
+        <el-button @click="cancelDataScope">取消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -132,6 +228,164 @@ export default {
   name: 'Role',
   data () {
     return {
+      // 遮罩层
+      loading: true,
+      // 总记录数
+      total: 0,
+      // 角色列表数据
+      roleList: [],
+      // 弹出层标题
+      title: '',
+      // 是否显示弹出层
+      open: false,
+      // 是否显示弹出层数据权限
+      openDataScope: false,
+      // 日期范围
+      dateRange: [],
+      // 状态数据字典
+      statusOptions: [],
+      // 数据范围选项
+      dataScopeOptions: [
+        {
+          value: '1',
+          label: '全部数据权限'
+        },
+        {
+          value: '2',
+          label: '自定义数据权限'
+        },
+        {
+          value: '3',
+          label: '本部门数据权限'
+        },
+        {
+          value: '4',
+          label: '本部门及以下数据权限'
+        },
+        {
+          value: '5',
+          label: '仅本人数据权限'
+        }
+      ],
+      // 菜单列表
+      menuOptions: [],
+      // 部门列表
+      deptOptions: [],
+      // 查询参
+      queryParams: {
+        pageNum: 1,
+        pageSize: 20,
+        roleName: undefined,
+        roleKey: undefined,
+        status: undefined
+      },
+      // 表单参数
+      form: {},
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      },
+      // 表单检验
+      rules: {
+
+      },
+      currentRow: null
+    }
+  },
+  created () {
+
+  },
+  computed: {
+    isSelected () {
+      return (this.currentRow == null) ? false : true
+    }
+  },
+  methods: {
+    // 查询角色列表
+    getList () {
+      this.loading = true
+      listRole(this.addDateRange(this.queryParams, this.dateRange)).then(
+        response => {
+          this.roleList = response.rows
+          this.total = response.total
+          this.loading = false
+        }
+      )
+    },
+    // 查询菜单树结构
+    getMenuTreeselect () {
+
+    },
+    // 查询部门树结构
+    getDeptTreeselect () {
+
+    },
+    // 所有菜单节点数据
+    getMenuAllCheckedKeys () {
+
+    },
+    // 所有部门节点数据
+    getDeptAllCheckedKeys () {
+
+    },
+    // 根据角色Id查询菜单树结构
+    getRoleMenuTreeselect (roleId) {
+
+    },
+    // 根据角色Id查询部门树结构
+    getRoleDeptTreeselect (roleId) {
+
+    },
+    // 角色状态修改
+    handleStatusChange (row) {
+
+    },
+    // 取消按钮
+    cancel () {
+
+    },
+    // 取消按钮 数据权限
+    cancelDataScope () {
+
+    },
+    // 表单重置
+    reset () {
+
+    },
+    // 搜索按钮操作
+    handleQuery () {
+
+    },
+    // 重置按钮操作
+    resetQuery () {
+
+    },
+    // 新增按钮操作
+    handleAdd () {
+
+    },
+    // 修改按钮操作
+    handleUpdate () {
+
+    },
+    // 分配数据权限操作
+    handleDataScope () {
+
+    },
+    // 提交按钮
+    submitForm () {
+
+    },
+    // 提交按钮 数据权限
+    submitDataScope () {
+
+    },
+    // 删除按钮操作
+    handleDelete () {
+
+    },
+    // 导出按钮操作
+    handleExport () {
 
     }
   }
